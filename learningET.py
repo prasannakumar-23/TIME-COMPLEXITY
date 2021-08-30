e8ad5871d6e9b1161175a4c1b78c957c60fca9e7
ANUSH MANGAL <74852516+ANUSHMANGAL01@users.noreply.github.com>
2021-08-30 21:53:49 +0530

    Add files via upload

--- learningET.py
+++ learningET.py
@@ -3,7 +3,7 @@ import xml.etree.ElementTree as ET
 import os
 os.system("srcml simple_demo.cpp -o input.xml")
 os.system("srcml simple_demo.cpp -o input.txt")
-tree = ET.parse('recursion.xml')
+tree = ET.parse('for_and_while.xml')
 root = tree.getroot()
 
 f = open("input.txt")
@@ -29,9 +29,11 @@ def handle_block(block, comp, method_name):
     for children in block_content:
         tag = str(children.tag)
         tag = tag[tag.index('}')+1 : ] 
-        print(tag, "block", "function " + method_name)   
+        # print(tag, "block", "function " + method_name)   
         if(tag=="if_stmt"):
-            handle_if_stmt(children, comp, method_name)    
+            handle_if_stmt(children, comp, method_name)   
+        if(tag=="for"):
+            handle_for(children, comp, method_name) 
     #TODO: add more statements
         
 
@@ -75,7 +77,7 @@ def handle_if(if_, comp, method_name):
     # TODO: write for if
     condition = if_[0]
     for call in condition.iter("{http://www.srcML.org/srcML/src}call"):
-        # TODO : handle_for
+        
         print(call[0].text, "Found a function call")
 
     block =if_[1]
@@ -92,6 +94,90 @@ def handle_else(else_, comp, method_name):
     block = else_[0]
     handle_block(block,comp, method_name)
 
+def handle_control(control, comp, method_name):
+    
+    increment = control[2]
+    increment =increment[0] # points to expr
+    exp1 =""
+    op1=""
+    for children in increment:
+        if children.tag == "{http://www.srcML.org/srcML/src}operator" and children.text != "=":
+            op1 = op1 +children.text
+        exp1 = exp1 +children.text
+    
+    exp1 = exp1[exp1.find(op1):]
+    exp1 =exp1[len(op1):] # op contains the operator and exp is the value
+    
+    exp2=""
+    op2=""
+    condition = control[1]
+    condition=condition[0]
+    for children in condition:
+        if children.tag == "{http://www.srcML.org/srcML/src}operator" and children.text != "+" and children.text != "-" and children.text != "*" and children.text != "/":
+            op2 = op2 +children.text
+        exp2 = exp2 +children.text
+    
+    exp2 = exp2[exp2.find(op2):]
+    exp2 =exp2[len(op2):] # op contains the operator and exp is the value
+
+    exp3=""
+    op3=""
+    initial = control[0]
+    initial=initial[0]
+    for children in initial:
+        if children.tag == "{http://www.srcML.org/srcML/src}operator" and children.text == "=":
+            op3 = op3 +children.text
+        exp3 = exp3 +children.text
+    
+    exp3 = exp3[exp3.find(op3):]
+    exp3 =exp3[len(op3):] # op contains the operator and exp is the value
+    print(op3, exp3, op2, exp2, op1, exp1)
+    
+    is_exp2_digit=True
+    for character in exp2:
+        if character.isalpha():
+            is_exp2_digit=False
+    is_exp3_digit=True
+    for character in exp3:
+        if character.isalpha():
+            is_exp3_digit=False
+
+    
+    # print(is_exp2_digit, is_exp3_digit)
+    if (not is_exp3_digit) or (not is_exp2_digit):
+        if ((op1=="+=" and exp1.isdigit()) or op1=="++" or op1=="--" or (op1=="-=" and exp1.isdigit()) or (op1=="+" and exp1.isdigit()) or(op1=="-" and exp1.isdigit()) ):
+            comp = "n"+comp
+            print(comp)
+            return comp
+        if ((op1=="*=" and exp1.isdigit()) or  (op1=="/=" and exp1.isdigit()) or (op1=="*" and exp1.isdigit()) or (op1=="/" and exp1.isdigit())):
+            comp = comp + "log(n)b(" + exp1 +")"
+            print(comp)
+            return comp
+    print(comp)
+    return comp
+    
+
+
+
+
+
+    # for ele in control.iter():
+    #     if(ele.tag=="{http://www.srcML.org/srcML/src}condition" or ele.tag=="{http://www.srcML.org/srcML/src}incr"):
+    #         exp = exp+";"
+        
+    #     if(ele.text):
+    #         exp = exp + ele.text
+    # exp = exp+")"
+    # start= exp[exp.index("=")+1 : exp.index(";")]
+
+
+    
+def handle_for(for_, comp, method_name):
+    control =for_[0]
+    comp = handle_control(control, comp, method_name)
+    
+    block =for_[1]
+    handle_block(block, comp, method_name)
 
 
 
